dc7aada11ab91532a5c27ac2fc53771f58eaa75b
mark-williams <markwill@hotmail.co.uk>
2018-08-26 11:35:10 +0100

    chore: styling - header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import Link from 'gatsby-link';
 
+const HeaderContainer = styled.div`
+  background: #000;
+  padding: 0;
+  margin-bottom: 2rem;
+  box-shadow: 0 0.2rem 0.8rem;
+`;
+
+const HeaderContent = styled.div`
+  color: hsl(0, 0%, 92%);
+  margin: 0;
+  font-size: 2rem;
+  padding: 1.4rem 2rem;
+  a {
+    color: hsl(0, 0%, 92%);
+  }
+`;
+
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem'
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderContainer>
+    <HeaderContent>
+      <Link to="/">Gatsby</Link>
+    </HeaderContent>
+  </HeaderContainer>
 );
 
 export default Header;
