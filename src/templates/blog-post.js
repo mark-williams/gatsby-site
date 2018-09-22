14cbb63647bc7ed885cb8c513cb62869fcb24f6e
mark-williams <markwill@hotmail.co.uk>
2018-09-22 12:06:42 +0100

    chore: implement template for blog post (TODO needs styling)

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 
 const Template = ({ data }) => {
+  const { markdownRemark: post } = data;
+  const { frontmatter, html } = post;
   return (
     <div>
-      <h2>This will be the template for a blog</h2>
-      <p>Watch this space...</p>
+      <h2>{frontmatter.title}</h2>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
+};
+
+Template.propTypes = {
+  data: PropTypes.object
 };
 
 export const pageQuery = graphql`
