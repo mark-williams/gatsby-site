c394098b19a746e90426f73ab4268cdf8293d9ad
mark-williams <markwill@hotmail.co.uk>
2018-09-22 11:36:13 +0100

    feat: display a list of blogs on a dedicated blogs page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import { Section } from '../components/Section';
 import { SectionBody } from '../components/SectionBody';
 
-const BlogPage = () => (
-  <Layout>
-    <Section>
-      <SectionBody>
-        <h1>Blog</h1>
-        <p>List of blogs here...</p>
-        <Link to="/">Go back to the homepage</Link>
-      </SectionBody>
-    </Section>
-  </Layout>
-);
+const BlogItem = ({ item }) => <div>{item.frontmatter.title}</div>;
+BlogItem.propTypes = {
+  item: PropTypes.object
+};
 
-export default BlogPage;
+const BlogsPage = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+  return (
+    <Layout>
+      <Section>
+        <SectionBody>
+          <h1>Blog</h1>
+          <div>
+            {posts.map(({ node }) => (
+              <BlogItem key={node.id} item={node} />
+            ))}
+          </div>
+          <Link to="/">Go back to the homepage</Link>
+        </SectionBody>
+      </Section>
+    </Layout>
+  );
+};
+
+BlogsPage.propTypes = {
+  data: PropTypes.object
+};
+
+export const query = graphql`
+  query BlogsQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMM YYYY")
+            path
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default BlogsPage;
